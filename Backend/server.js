4f52c34afd7234a10f55513b8d557635751c8d74
unknown <dwiprajdey33720@gmail.com>
2026-08-20 12:03:46 +0530

    added the frontend url

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true,
+}));
 app.use(express.json());
 
 // session + passport must come BEFORE routes that use passport
@@ -43,4 +46,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,  '0.0.0.0',() => console.log(`Server running on port ${PORT}`));
